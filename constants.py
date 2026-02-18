2f6e71ab53c0752428b37f8abc85d75cffb79bba
heagenb03 <heagenbell@outlook.com>
2026-02-17 18:49:00 -0600

    New file: Added constants.py for project-wide constants

---
+++ constants.py
@@ -0,0 +1,19 @@
+"""
+
+Constants used across the project
+
+Usage:
+    from constants import XYZ
+
+"""
+
+# Keywords
+
+CHINA_KEYWORDS = [
+    "china", "chinese", "prc", "people's republic of china", "beijing", "shanghai", "shenzhen",
+    "guangzhou", "national people's congress", "npc", "politburo", "chinese communist party", "ccp",
+    "communist party of china", "state council of china", "pboc", "people's bank of china", "mofcom",
+    "ministry of commerce china", "ministry of foreign affairs china", "mfa china", "mss",
+    "ministry of state security", "pla", "people's liberation army", "pla navy", "pla air force", "rocket force",
+    "xi jinping", "li keqiang", "li qiang", "hu jintao", "xinhua", "people's daily", "cgtn",
+]
